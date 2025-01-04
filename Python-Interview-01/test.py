3b07c3af7b5b773e2144e0b0a8ffc7c057d33c7c
Th-Yash <thakoryashpal755@gmail.com>
2025-01-04 11:24:06 +0530

    neww test fill add

--- Python-Interview-01/test.py
+++ Python-Interview-01/test.py
@@ -1,34 +1,41 @@
-#Q 1
+# #Q 1
 
 
-class Animes:
-    def __init__(self,Anime_name,Rating,Episodes):
-        self.Anime_name=Anime_name
-        self.Rating=Rating
-        self.Episodes=Episodes
+# class Animes:
+#     def __init__(self,Anime_name,Rating,Episodes):
+#         self.Anime_name=Anime_name
+#         self.Rating=Rating
+#         self.Episodes=Episodes
     
-    def Laanime(Self):
-        print(f'Which anime do you like {Self.Anime_name} ? that have rating of {Self.Rating}  adn howmany episodes {Self.Episodes}')
+#     def Laanime(Self):
+#         print(f'Which anime do you like {Self.Anime_name} ? that have rating of {Self.Rating}  adn howmany episodes {Self.Episodes}')
 
-anime_A = Animes("Naruto",8.5,100)
-anime_B = Animes('bluelock',7.5,21)
+# anime_A = Animes("Naruto",8.5,100)
+# anime_B = Animes('bluelock',7.5,21)
 
-anime_A.Laanime()
-anime_B.Laanime()   
+# anime_A.Laanime()
+# anime_B.Laanime()   
 
 
-# Q 2
+# # Q 2
 
-class NumbersStr:
+# class NumbersStr:
     
-    def check_numbers(self):
-        n = 0
-        for n in range(1,50):
-            if n % 2:
-                print(f'This is an odd number: {n}')
-            else:
-                print(f'This is an even number: {n}')
+#     def check_numbers(self):
+#         n = 0
+#         for n in range(1,50):
+#             if n % 2:
+#                 print(f'This is an odd number: {n}')
+#             else:
+#                 print(f'This is an even number: {n}')
 
-# Create an instance of the class and call the method
-numbers = NumbersStr()
-numbers.check_numbers()
+# # Create an instance of the class and call the method
+# numbers = NumbersStr()
+# numbers.check_numbers()
+
+
+
+# Q 3 
+
+with open('Test2.py','w') as file:
+    file.write("")
