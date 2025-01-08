0e78d2de9754fa22dbcdcdf6b9197e0d9366ccf3
Th-Yash <thakoryashpal755@gmail.com>
2025-01-08 16:12:09 +0530

    update this pytorch

--- Python-Py-Torch/main.py
+++ Python-Py-Torch/main.py
@@ -1,44 +1,40 @@
 import numpy as np
 import torch as pytorch
 
+def pytorh():
+    a = np.ones(10)
+    b = np.ones(10)* 2
+    c = b * a
+    d = c + 1
+    print(d,c)
+    
+pytorh()    #pip install torch torchvision torchaudio
 
 
+# second exapmle
+
+def one():
+    A = ("A")
+    B = ("B")
+    C = A * B
+    D = C +(1)
+    F = compile (D)
+    d = F(A=np.ones(10),B=np.ones(10)*2)
+
+one()  
 
 
-# # def pytorh():
-# #     a = np.ones(10)
-# #     b = np.ones(10)* 2
-# #     c = b * a
-# #     d = c + 1
-# #     print(d,c)
+# Define x as a tensor with gradient tracking enabled
+x = pytorch.tensor(1.0, requires_grad=True)
 
-# # pytorh()    pip install torch torchvision torchaudio
+# Define y as a function of x
+y = 4 * x + 3
 
+# Perform backward pass to compute the gradient of y w.r.t. x
+y.backward()
 
-
-# # # second exapmle
-# # def one():
-# #     A = ("A")
-# #     B = ("B")
-# #     C = A * B
-# #     D = C +(1)
-# #     F = compile (D)
-# #     d = F(A=np.ones(10),B=np.ones(10)*2)
-
-# # one()  
-
-
-# # Define x as a tensor with gradient tracking enabled
-# x = pytorch.tensor(1.0, requires_grad=True)
-
-# # Define y as a function of x
-# y = 4 * x + 3
-
-# # Perform backward pass to compute the gradient of y w.r.t. x
-# y.backward()
-
-# # Print the gradient of x
-# print(x.grad)
+# Print the gradient of x
+print(x.grad)
 
 
 class TensorCount:
@@ -58,3 +54,4 @@ la_ma = TensorCount(count=5, tensor=None, cpu_usage=50, gpu_usage=75)
 
 # Call the method
 la_ma.count_number()
+
