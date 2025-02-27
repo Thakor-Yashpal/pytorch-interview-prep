ed028f96ead3662a6f2cb6aef92e87597b2af205
Th-Yash <thakoryashpal755@gmail.com>
2025-02-27 16:08:50 +0530

    tensor creating
     We are creating the tensor using a small dataset, but this will not affect the overall project.
    The approach is intended for testing and will not impact the final implementation or results.

---
+++ PY-Torch ex/learn_torch.py
@@ -0,0 +1,22 @@
+import torch 
+
+# scaler = torch.tensor(7)
+# scaler.ndim
+# scaler.item()
+# print(scaler.item())
+
+vactor = torch.tensor([7,7])
+vactor.ndim
+vactor.shape   # A tensor with 2 elements cannot be converted to scalar
+
+print(vactor)
+
+matrex = torch.tensor(  [[7,7],
+                        [12,12],
+                        [14,14]])
+
+print(matrex[1])
+
+
+# creating a tensor with small data 
+
