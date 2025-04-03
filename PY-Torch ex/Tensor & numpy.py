fa4b81ae316286a1bb19758c83cf2ffbad6de598
Thakor-Yashpal <thakoryashpal755@gmail.com>
2025-04-03 13:57:04 +0530

    new fille

---
+++ PY-Torch ex/Tensor & numpy.py
@@ -0,0 +1,43 @@
+import torch
+import numpy
+import numpy as np 
+
+
+# Numpy array vs pytorch tensor 
+
+# Tensor = torch.tensor([1,2,3], device= 'cpu')
+
+# Array = numpy.array([1,2,3])
+
+
+# print (Tensor)
+
+# print(Array)
+
+
+#------------ numpy array for createing random numbers for tesnsor ----------
+
+
+
+# --------Tesnsor 2d and 3d,how to createa tensor------------
+
+# tensor_2d =torch.randn(3,4)
+
+# print(tensor_2d)
+
+# tensor_3d =torch.zeros(2,3,4)
+
+# print(tensor_3d)
+
+
+# createa tensor out of numpy array
+
+np1 =np.random.rand(3,4)
+
+VL01 = torch.from_numpy(np1)
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+VL02 = VL01.to(dtype=torch.float64, device=device)
+
+print(device)
+
